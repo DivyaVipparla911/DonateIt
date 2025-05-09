660d7af368f7644065de7127ca911edf4c68578e
DivyaVipparla911 <dv06610n@pace.edu>
2025-05-09 01:41:17 -0400

    removed config

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7npo_-9KAwTJCxwRtzgceJt4_bostfss",
-    authDomain: "donateit-80076.firebaseapp.com",
-    projectId: "donateit-80076",
-    storageBucket: "donateit-80076.firebasestorage.app",
-    messagingSenderId: "916451582794",
-    appId: "1:916451582794:web:efb23b8735dad5dd5217b3",
-    measurementId: "G-D999GEXDBT"
+   
 };
 
 const app = initializeApp(firebaseConfig);
